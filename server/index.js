156aa7d227a45e361a5fbccef97573f01bbb0c7f
Aditya Sharma <adityasharma2005123@gmail.com>
2025-03-16 18:28:34 +0530

    Made the Correct API Endpoints and Auth Logic

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const app = express();
 
 const port = process.env.PORT;
 
+const contestRoutes = require("./routes/contest");
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.json({
         message: "Welcome to TLE CP Tracker!"
     });
-})
+});
+
+app.get("/api", contestRoutes);
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
--- server/routes/contest.js
+++ server/routes/contest.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { getCodeforcesContests } = require('../controllers/contestController');
+// const verifyToken = require('../middlewares/auth');
 
 // router.get('/codeforces', verifyToken, getCodeforcesContests);
 
