6e429ac6db05955d62dfdd1cd3dd5a2e1f85cbb8
Aditya Sharma <adityasharma2005123@gmail.com>
2025-03-16 23:07:48 +0530

    Added Customized Endpoints for CodeForces Upcoming and Past Contests

--- server/routes/contest.js
+++ server/routes/contest.js
@@ -5,12 +5,14 @@ const { getCodeforcesContests } = require('../controllers/codeforcesController')
 const { getUpcomingCodeforcesContests } = require('../controllers/upcomingCodeforcesController');
 const { getUpcomingCodechefContests } = require('../controllers/upcomingCodechefController');
 const { getPastCodechefContests } = require('../controllers/pastCodechefController');
+const { getPastCodeforcesContests } = require('../controllers/pastCodeforcesController');
 // const verifyToken = require('../middlewares/auth');
 
 // router.get('/codeforces', verifyToken, getCodeforcesContests);
 
 router.get('/codeforces', getCodeforcesContests);
 router.get('/upcomingcodeforces', getUpcomingCodeforcesContests);
+router.get('/pastcodeforces', getPastCodeforcesContests);
 router.get('/upcomingcodechef', getUpcomingCodechefContests);
 router.get('/pastcodechef', getPastCodechefContests);
 
